7cf4608f63db05c8a3f29ead9f73bfbd346e8bc1
Anik Hossain <anik.wdev@gmail.com>
2021-11-29 19:07:42 +0600

    boostrap and axios setup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
 import App from "./components/App.vue";
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(router, axios, VueAxios, store).mount("#app");
